02f2180bff3aba771c061dd449582fa7cc20c523
rasyidaskara <arik.sofan.tohir@gmail.com>
2025-10-01 09:32:26 +0700

    Initial commit

---
+++ demo.js
@@ -0,0 +1,6 @@
+function kurang(){
+    var v1 = document.getElementById('v1').value;
+    var v2 = document.getElementById('v2').value;
+    var x = parseInt(v1) - parseInt(v2);
+    document.getElementById("hasil").value = x;
+}
